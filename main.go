d7f8dd52bf5599bc3a12f301ca01b5c15374b443
tacheraSasi <tacherasasi@gmail.com>
2026-01-02 15:30:35 +0300

    addd

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 const asciiHead = `
 _______  _______  ______  
 |\     /|(  ____ \(  ___  )(  __  \ 
@@ -25,10 +29,18 @@ _________ _______ _________ _       _______
 `
 
 func main(){
-	println("flippp")
+	display("head")
 }
 
 
-func display(){
+func display(output string){
+	switch output {
+		case "head":
+			fmt.Printf(asciiHead)
+		case "tails":
+			fmt.Printf(asciiTails)
+		default:
+			fmt.Println("Soemthing Went Wrong")
+	}
 	
 }
